04880d41c2c33e23ecbd4d8992db9839b11d685e
Chau Tran <nartc7789@gmail.com>
2020-10-26 22:24:11 -0500

    add primeng

--- apps/web/src/app/app.component.ts
+++ apps/web/src/app/app.component.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 import { of } from 'rxjs';
 
 @Component({
   selector: 'post-rest-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   hello$ = of({ message: 'welcome to api' });
 
-  constructor(private http: HttpClient) {
+  constructor(private readonly primeNgConfig: PrimeNGConfig) {}
+
+  ngOnInit() {
+    this.primeNgConfig.ripple = true;
   }
 }
--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -2,13 +2,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { API_BASE_URL } from '@post-rest-web/api-stub';
+import { RippleModule } from 'primeng/ripple';
 import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, RippleModule],
   providers: [{ provide: API_BASE_URL, useValue: environment.apiUrl }],
   bootstrap: [AppComponent],
 })
